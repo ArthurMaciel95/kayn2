38701d449a595003201765a94ec35ce4d85e8b89
ArthurMaciel95 <arthurnmrocha@gmai.com>
2021-02-06 11:10:52 -0300

    change logo

--- server.js
+++ server.js
@@ -4,27 +4,22 @@ const app = require("./app");
 require("dotenv").config();
 const PORT = process.env.PORT || 4455;
 
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-    console.log("running...");
+require("./src/models/PostModel");
+
+mongoose
+    .connect(process.env.DATABASE, {
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("conectado à base de dados");
+        app.emit("pronto");
+    })
+    .catch((e) => console.log(e));
+
+app.on("pronto", () => {
+    app.listen(PORT, () => {
+        console.log(`http://localhost:${PORT}`);
+    });
 });
-
-// require('./src/models/Post');
-
-// mongoose
-//   .connect(process.env.DATABASE, {
-//     useFindAndModify: false,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log('conectado à base de dados');
-//     app.emit('pronto');
-//   })
-//   .catch((e) => console.log(e));
-
-// app.on('pronto', () => {
-//   app.listen(port, () => {
-//     console.log(`http://localhost:${port}`);
-//   });
-// });
