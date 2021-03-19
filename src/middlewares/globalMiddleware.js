7fcff04060459209243a28c831d3e6d22c78f938
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-03-19 01:20:06 -0300

    update desing page profile

--- src/middlewares/globalMiddleware.js
+++ src/middlewares/globalMiddleware.js
@@ -1,6 +1,18 @@
 const { helpers } = require("../helpers/navbarLinks");
 
 exports.middlewareGlobal = (req, res, next) => {
+
+    if(req.session.user){
+        helpers.filter( helper => helper.logged === true)
+        console.log('logged');
+    } else {
+        helpers.filter( helper => helper.guest === true)
+        console.log('notlogged');
+    }
+
+
+
+
     res.locals._user = req.session.user
     res.locals.h = helpers;
     res.locals.error = req.flash('error');
